711654a1eb789993915454644c44ab95d5badcef
richard_glb <kilr1892@163.com>
2019-10-18 10:19:32 +0800

    Initial commit

---
+++ src/main/java/class01/my/BubbleSort.java
@@ -0,0 +1,128 @@
+package class01.my;
+
+import java.util.Arrays;
+
+/**
+ * 沉石法(冒泡法变体).
+ * 思路:
+ *      从左往右, 两两比较, 大的右移
+ * 复杂度:
+ *      最坏时间 O(n^2)
+ *      最优时间 O(n^2)
+ *      平均时间 O(n^2)
+ *
+ *      最坏空间 共O(n), 辅助空间O(1)
+ * <p>
+ * When I wrote this, only God and I understood what I was doing
+ * Now, God only knows
+ *
+ * @author RichardLee
+ * @version v1.0
+ */
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+
+        // 依次两两比较 大的右移 沉石法
+        for (int i = arr.length - 1; i > 0; i--) {
+            // i 控制最后那个需要排序的位置
+            // 比如a[3,1,5] i要从2 开始, 等于1停止, 即小于i
+            for (int j = 0; j < i; j++) {
+                // j 控制从头一直比较到i
+                // j 从0开始, 一直到i-1为止, 因为要和j+1比
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
+
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
+
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
+}
