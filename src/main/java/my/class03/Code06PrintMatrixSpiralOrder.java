76638583dd3b411c5e2cf2e020b63354e9236308
richard_glb <kilr1892@163.com>
2019-10-23 14:45:30 +0800

    顺时针回形打印矩阵 加了注释

--- src/main/java/my/class03/Code06PrintMatrixSpiralOrder.java
+++ src/main/java/my/class03/Code06PrintMatrixSpiralOrder.java
@@ -1,7 +1,12 @@
 package my.class03;
 
 /**
- * .
+ * 转圈打印矩阵
+ * 【题目】 给定一个整型矩阵matrix，请按照转圈的方式打印它。
+ * 例如： 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+ * 15 16 打印结果为：1，2，3，4，8，12，16，15，14，13，9，
+ * 5，6，7，11， 10
+ * 【要求】 额外空间复杂度为O(1)。
  *
  * @author RichardLee
  * @version v1.0
@@ -25,6 +30,7 @@ public class Code06PrintMatrixSpiralOrder {
         // 列数
         int dY = matrix[0].length - 1;
         while (tX <= dX && tY <= dY) {
+            // 打印好后, 将边框的两个坐标依次修改
             printMatrix(matrix, tX++, tY++, dX--, dY--);
         }
 
@@ -32,28 +38,36 @@ public class Code06PrintMatrixSpiralOrder {
 
     public static void printMatrix(int[][] matrix, int tX, int tY, int dX, int dY) {
         if (tX == dX) {
+            // 这个是在只有一列的情况下的打印
             for (int i = tY; i <= dY; i++) {
                 System.out.print(matrix[tX][i] + " ");
             }
         } else if (tY == dY) {
+            // 这个是在只有一行情况下的打印
             for (int i = tX; i <= dX; i++) {
                 System.out.print(matrix[i][tY] + " ");
             }
         } else {
+            // 这个是打印外边框
+            // 当前的xy坐标
             int indexX = tX;
             int indexY = tY;
+            // 最上面行, 打印到倒数第二个数
             while (indexY < dY) {
                 System.out.print(matrix[indexX][indexY] + " , ");
                 indexY++;
             }
+            // 最右边列, 打印到倒数第二个数
             while (indexX < dX) {
                 System.out.print(matrix[indexX][indexY] + " , ");
                 indexX++;
             }
+            // 最下面行, 从右往左打印到第二个数
             while (indexY > tX) {
                 System.out.print(matrix[indexX][indexY] + " , ");
                 indexY--;
             }
+            // 最左边列, 从下往上打印到第二个数
             while (indexX > tY) {
                 System.out.print(matrix[indexX][indexY] + " , ");
                 indexX--;
